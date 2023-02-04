ace0a995523b9844c808857f93be47e969e315fd
Radiopapus <4965501+radiopapus@users.noreply.github.com>
2023-02-04 14:26:42 +0200

    Добавил HELP команду (#37)
    #I34-add-help-command
    
    Добавил команду HELP. Выводит краткую информацию о mashinka.
    
    Использовал colored crate для раскрашивания текста.
    
    Добавил описание переменных окружения в README.md
    
    Добавил тест для HELP команды.
    
    ---------
    
    Co-authored-by: Radiopapus <viktor@zharina.info>

--- src/command/help.rs
+++ src/command/help.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::must_use_candidate)]
 
-use crate::command::{Command, CommandResult, Details, Error, HELP_COMMAND_NAME};
+use colored::Colorize;
+use crate::command::{Command, CommandResult, Error};
 
 pub struct Help;
 
@@ -12,9 +13,20 @@ impl Help {
 
 impl Command for Help {
     fn run(&self) -> Result<CommandResult, Error> {
-        Ok(CommandResult {
-            command: HELP_COMMAND_NAME.to_string(),
-            details: Details::new()
-        })
+        println!("{} {}", "Usage:".green().bold(), "mashinka CMD --arg-name --argv-name=value".blue());
+        println!("{} {}", "Example:".green().bold(), "mashinka publish --dry-run".blue());
+        println!();
+        println!("{}", "Available commands:".green().bold());
+        println!("{} - uses draft file as a source of content and create to post and translation
+based on specified lang.", "publish".blue());
+        println!("{} - builds data file for indexing system (elasticlunr) based on posts content.", "index".blue());
+        println!("{} - uploads and extract data to cloud storage (selectel for now).", "deploy".blue());
+        println!("{} - shows content from HELP.md file.", "help".blue());
+        println!("{} - shows mashinka version.", "version".blue());
+        println!();
+        println!("{}", "Miscellaneous:".green().bold());
+        println!("{} - runs command in so called dry-run mode( without side artifacts).", "--dry-run".blue());
+
+        Ok(CommandResult::default())
     }
 }
